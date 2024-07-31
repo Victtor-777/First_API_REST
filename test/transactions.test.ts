5d78a3fceba157c00c8dcb08eb4eb7abab015b5b
Victtor777 <victtor.eng.software@gmail.com>
2024-07-31 14:53:59 -0300

    Configurando o banco de testes

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,14 +1,22 @@
-import { afterAll, beforeAll, it, describe, expect } from "vitest";
+import { afterAll, beforeAll, it, describe, expect, beforeEach } from "vitest";
 import request from "supertest";
+import { execSync } from "node:child_process";
 import { app } from "../src/app";
 
 describe("Transactions routes", () => {
   beforeAll(async () => {
+    execSync("npm run knex migrate:latest");
+
     await app.ready();
   });
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
   });
 
   it("User can create a new transaction", async () => {
